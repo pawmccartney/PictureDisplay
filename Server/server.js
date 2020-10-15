92ad831c3dff6c6ddf41181ac9bc88235e8242d4
Jason Strunk <jasondstrunk@gmail.com>
2020-10-15 15:25:02 -0700

    Post endpoint created, not yet tested

--- Server/server.js
+++ Server/server.js
@@ -3,24 +3,31 @@ const Hotel = require('./../Database/Photo.js')
 const app = express();
 const port = 4000;
 
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 app.use(express.static(__dirname + '/../public/'));
 app.use('/:hotelId', express.static(__dirname + '/../public/'));
 app.use(express.json());
 
 app.get('/api/pictures/:hotel', (req, res) => {
   let data = req.params.hotel;
-  console.log(data);
   Hotel.find({ name: data})
   .then((results) => {
     res.send(results);
   })
 })
 
-// app.get('/', (req, res) => {
-
-//     res.send('HELLO')
-
-// })
+app.post('/', ( req, res ) => {
+  Hotel.create(req.body)
+  .then( function(result) {
+    res.send(result);
+  })
+})
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
